749b0265cdc98d89a3aba023f5ae6b6cd70f5c20
Ansh633 <ansh4763.be23@chitkara.edu.in>
2024-09-15 15:40:25 +0530

    Add files via upload

---
+++ SignUpForm.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const SignUpForm = () => {
+  return (
+    <div className="sign-up">
+      <h2>Sign Up for our Daily Insider</h2>
+      <input type="email" placeholder="Enter your email" />
+      <button>Subscribe</button>
+    </div>
+  );
+};
+
+export default SignUpForm;
